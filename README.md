1213a3f28d0c925f5d335fb6fca81ae72912e5eb
Adrien Hopkins <ahopk127@my.yorku.ca>
2020-10-24 18:02:01 -0500

    Reorganized repository
    Put data files in a data/ folder

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 This is Team Rocket's entry in the YEC Programming Competition - 2020 Fall
+
+To run the code, simply execute 'specimen_f.py'.
--- __main__.py
+++
@@ -1,6 +0,0 @@
-from pprint import *
-from specimen_f import *
-
-filename = input("Enter input filename: ")
-fission = input("Enable fission? [Y/N] ").capitalize().startswith('Y')
-run_file(filename, True, fission)
